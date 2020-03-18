5794d76f2a38e537c9760818d4a317d712fd9b63
Yuliya Osmolovskaya <yuliya.asmalouskaya@gmail.com>
2020-03-18 18:02:52 +0100

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+  let strArray = str.split("");
+  for (let i = 0; i < strArray.length; i++) {
+    for (let k = 0; k < bracketsConfig.length; k++) {
+      if (strArray[i] === bracketsConfig[k][0] &&
+        strArray[i + 1] === bracketsConfig[k][1]) {
+        strArray.splice(i, 2);
+        i = -1;
+      }
+
+      if (strArray.length === 0) {
+        return true;
+      }
+    }
+  }
+  return false;
 }
